bdddc32b615d5e80d5ead9a689f9bc37c505f75c
SAYED <81683385+SK-SAYED-AKTAR@users.noreply.github.com>
2021-09-17 16:48:33 +0530

    Update settings.py

--- ProjectOCR/settings.py
+++ ProjectOCR/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2he4)@j%$0qulqck0@rdi8t1)mel=vt5uzk$bjf82^+hyk())_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://go-pronto.herokuapp.com/", "localhost", "127.0.0.1"]
 
 
 # Application definition
